5b4e4ce90ea278c7c857e83e8f45ab601d2bbac1
petriken <petriken82@gmail.com>
2019-03-15 00:17:00 +0300

    feat: add feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const openBrackets = [];
+  const closeBrackets = [];
+  const allBrackets = [];
+  const equalBrackets = [];
+  let resultArray = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openBrackets.push(bracketsConfig[i][0]);
+    closeBrackets.push(bracketsConfig[i][1]);
+    allBrackets.push(bracketsConfig[i][0]);
+    allBrackets.push(bracketsConfig[i][1]);
+    if (bracketsConfig[i][0] === bracketsConfig[i][1]) {
+      equalBrackets.push(bracketsConfig[i][0]);
+    }
+  }
+  // проверка на четность скобок
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    let current = str[i];
+    let previous = resultArray[resultArray.length - 1];
+
+    if (openBrackets.indexOf(current) != -1 && equalBrackets.length === 0) {
+      resultArray.push(current);
+    } else if (
+      current === allBrackets[allBrackets.indexOf(previous) + 1] &&
+      equalBrackets.length !== 0
+    ) {
+      resultArray.pop();
+    } else if (
+      openBrackets.indexOf(current) != -1 &&
+      equalBrackets.length !== 0
+    ) {
+      resultArray.push(current);
+    } else if (
+      current === allBrackets[allBrackets.indexOf(previous) + 1] &&
+      equalBrackets.length === 0
+    ) {
+      resultArray.pop();
+    }
+  }
+  if (resultArray.length === 0) {
+    return true;
+  }
+  return false;
+};
